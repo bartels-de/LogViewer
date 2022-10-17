b177f3138e4c37dba216ec334248d47bb626f39e
Jannik Bartels <66883009+bartels-de@users.noreply.github.com>
2022-10-18 01:29:42 +0200

    feat(LogViewer): Added constants for regex matching

---
+++ LogViewer.Constants/RegexConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogViewer.Constants
+{
+    internal static class RegexConstants
+    {
+        public const string Date = "\\d{4}-\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}.\\d{4}";
+        public const string ThreadId = "[\\d]";
+        public const string LogLevel = "(TRACE|DEBUG|INFO|NOTICE|WARN|WARNING|ERROR|SEVERE|FATAL)";
+        public const string ClassName = "\\[[^\\]]*\\D]";
+    }
+}
